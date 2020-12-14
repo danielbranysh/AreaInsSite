c8a685037b4cd90b1dc934325762df92f22fd4fc
Daniel I Branysh <branysh_d65638@utpb.edu>
2020-12-14 13:30:33 -0600

    Add files via upload

---
+++ FINAL/process.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to the Newsletter!</title>
+</head>
+<body>
+<h1>
+Thank you! You signed up with the following e-mail:<?php echo $_POST["email"]; ?>
+	<br>
+</h1>
+</body>
+</html>
